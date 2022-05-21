a72fde0e391d378a8c129cd0d61ebad9bf56b607
Aiota120 <70962181+malia120@users.noreply.github.com>
2022-05-21 16:22:16 +0100

    Binary tree :)
    Learning how to use binary trees in my therapist :)

---
+++ TherapistTree.java
@@ -0,0 +1,15 @@
+import org.w3c.dom.Node;
+
+class TherapistTree {
+    String value;
+    Node left;
+    Node right;
+
+    public void Node(String value) {
+        this.value = value;
+        right = null;
+        left = null;
+
+    }
+    
+}
